2dbbf0928840edae47285811edd3606f5f1a4207
François DEMORY <demory.f@me.com>
2022-06-24 11:53:31 +0200

    add content and JS chart drawing

---
+++ js/script.js
@@ -0,0 +1,26 @@
+import datas from '../data.json' assert {type : 'json',};
+
+function drawChart() {
+    const chart = document.querySelector('.main__chart');
+    datas.forEach(data => {
+        const container = document.createElement('div');
+        const day = document.createElement('div');
+        const chartDatas = document.createElement('div');
+
+        container.classList.add('chart__container');
+        day.classList.add('chart__day');
+        chartDatas.classList.add('chart__datas');
+
+        day.innerHTML = `${data.day}`;
+        const chartHeight = (data.amount * 300)/100;
+        chartDatas.style.height = chartHeight + 'px';
+
+        chart.appendChild(container);
+        container.appendChild(chartDatas);
+        container.appendChild(day);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    drawChart();
+});
