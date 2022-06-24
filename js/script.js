8f8dd43f8566a70baaf2bf166d4f5962db14a438
François DEMORY <demory.f@me.com>
2022-06-24 12:22:40 +0200

    add mouseover and mouseout effects

--- js/script.js
+++ js/script.js
@@ -6,22 +6,32 @@ function drawChart() {
         const container = document.createElement('div');
         const day = document.createElement('div');
         const chartDatas = document.createElement('div');
+        const chartAmountModal = document.createElement('div');
 
         container.classList.add('chart__container');
         day.classList.add('chart__day');
         chartDatas.classList.add('chart__datas');
+        chartAmountModal.classList.add('chart__datas__modal');
 
         day.innerHTML = `${data.day}`;
-        const chartHeight = (data.amount * 300)/100;
-        chartDatas.style.height = chartHeight + 'px';
+        chartAmountModal.innerHTML = `$${data.amount}`;
+        chartDatas.style.height = (data.amount * 300)/100 + 'px';
 
         if (day.innerHTML === 'wed') {
             chartDatas.classList.add('chart__datas--wed');
         }
 
         chart.appendChild(container);
+        container.appendChild(chartAmountModal);
         container.appendChild(chartDatas);
         container.appendChild(day);
+
+        chartDatas.addEventListener('mouseover', () => {
+                chartAmountModal.style.visibility = 'visible';
+        });
+        chartDatas.addEventListener('mouseout', () => {
+                chartAmountModal.style.visibility = 'hidden';
+        });
     });
 }
 
